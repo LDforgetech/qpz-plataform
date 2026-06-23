6e3b213042f7e74e49695f7529a14e1e75bdf907
Dimitri13Freitas <freitasdimitri7@gmail.com>
2026-06-23 06:44:15 -0300

    deploy: settin up new config for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://qpz-plataform.b-cdn.net/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qpz-plataform-prod.b-cdn.net", // O domínio exato da Pull Zone de produção
+        port: "",
+        pathname: "/**", // Permite qualquer subpasta ou arquivo dentro da CDN
+      },
+      {
+        protocol: "https",
+        hostname: "qpz-plataform.b-cdn.net", // Mantive o sem prod também por segurança, caso use em dev
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
-  /* config options here */
+  /* outras opções de configuração aqui */
 };
 
 export default nextConfig;
